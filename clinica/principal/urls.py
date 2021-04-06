ce7158beb657db6779bb644ee79ebb28fe628407
alfredo <ajsnsn93@gmail.com>
2021-04-06 17:34:40 -0600

    Se agrego la view login y se añadio su path en app principal

---
+++ clinica/principal/urls.py
@@ -0,0 +1,8 @@
+"""Principal"""
+from django.urls import path
+
+from . import views
+
+urlpatterns = [
+    path('',views.login, name='login'),
+]
--- clinica/principal/views.py
+++ clinica/principal/views.py
@@ -1,3 +1,7 @@
+"""Views principal"""
 from django.shortcuts import render
 
 # Create your views here.
+def login(request):
+    """Login"""
+    return render(request,'principal/login.html')
